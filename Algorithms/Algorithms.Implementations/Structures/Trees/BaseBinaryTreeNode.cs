ee219c82afac69fb03b7e1eb26b6c3e1e7282023
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-06-23 16:04:17 +0300

    Implemented Direct Right node structure

---
+++ Algorithms/Algorithms.Implementations/Structures/Trees/BaseBinaryTreeNode.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Implementations.Structures.Trees
+{
+    /// <summary>
+    /// Base class for the binary tree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class BaseBinaryTreeNode<T>
+    {
+        protected BaseBinaryTreeNode(T curValue)
+        {
+            Value = curValue;
+        } 
+        /// <summary>
+        /// Returns subitems
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IEnumerable<BaseBinaryTreeNode<T>> GetSubItems();
+
+        /// <summary>
+        /// Current node Value
+        /// </summary>
+        public T Value { get;  private set; }
+         
+        /// <summary>
+        /// Level of the node
+        /// </summary>
+        protected uint Level { get; set; }
+
+
+        /// <summary>
+        /// Fill level of the current node and of this child
+        /// Level starts from the current node
+        /// </summary>
+        protected void FillLevels()
+        {
+            FillLevels(null);
+        }
+
+        /// <summary>
+        /// Fill levels
+        /// </summary>
+        /// <param name="parentLevel"></param>
+        private void FillLevels(uint? parentLevel)
+        {
+            Level = parentLevel + 1 ?? 0;
+            foreach (var item in GetSubItems())
+            {
+                item.FillLevels(Level);
+            }
+        }
+
+        /// <summary>
+        /// Returns enumerable
+        /// </summary>
+        /// <typeparam name="TU"></typeparam>
+        /// <returns></returns>
+        protected IEnumerable<TU> AsEnumerable<TU>() where  TU: BaseBinaryTreeNode<T>
+        {
+            yield return (TU)this;
+            foreach (var subItem in GetSubItems())
+            {
+                foreach (var  childs in subItem.AsEnumerable<TU>())
+                {
+                    yield return childs;
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Structures/Trees/DirectRightNode.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Implementations.Structures.Trees
+{
+    /// <summary>
+    /// Binary Tree node with link to the direct right
+    /// Direct right - is the next right node at the same level
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DirectRightNode<T>: BaseBinaryTreeNode<T>
+    {
+
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public DirectRightNode(T value):base(value)
+        {
+            _subItems = new Pair<DirectRightNode<T>>();
+        }
+        #endregion
+
+        #region Fields
+        /// <summary>
+        /// Pair of the child items
+        /// </summary>
+        private readonly Pair<DirectRightNode<T>> _subItems;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Node that direct right for current
+        /// </summary>
+        public DirectRightNode<T> DirectRight { get; private set; }
+
+        /// <summary>
+        /// Left child node
+        /// </summary>
+        public DirectRightNode<T> Left
+        {
+            get { return _subItems.Left; }
+            set { _subItems.Left = value; }
+        }
+
+        /// <summary>
+        /// Right child node
+        /// </summary>
+        public DirectRightNode<T> Right
+        {
+            get { return _subItems.Right; }
+            set { _subItems.Right = value; }
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Fill Direct right nodes 
+        /// using levels
+        /// </summary>
+        public void FillDirect()
+        {
+            FillLevels();
+            foreach (var level in AsEnumerable<DirectRightNode<T>>().GroupBy(x=>x.Level).Select(g=>g.ToList()))
+            {
+                for (var i = 0; i < level.Count-1; i++)
+                {
+                    level[i].DirectRight = level[i + 1];
+                }
+            }
+        }
+
+
+        #region BaseBinaryTreeNode imlementation
+        /// <summary>
+        /// Return subitems
+        /// </summary>
+        /// <returns></returns>
+        protected override IEnumerable<BaseBinaryTreeNode<T>> GetSubItems()
+        {
+            return _subItems;
+        }
+        #endregion
+        #endregion
+
+        public IEnumerable<DirectRightNode<T>> AsEnumerable()
+        {
+            return AsEnumerable<DirectRightNode<T>>();
+        }
+    }
+}
