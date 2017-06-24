6421b5591c69b936a821eb5b17515d7b76f4bc22
RuslanLatypov <rus_91.08@mail.ru>
2017-06-24 17:44:11 +0300

    solved birthday chocolate challenge

---
+++ Algorithms/Algorithms.Hackerrank.Solutions/Implementations/BirthDayChocolate/WaysCounter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Hackerrank.Solutions.Implementations.BirthDayChocolate
+{
+    /// <summary>
+    /// Solution for https://www.hackerrank.com/challenges/the-birthday-bar
+    /// </summary>
+    public class WaysCounter
+    {
+        private readonly byte[] _chocolate;
+
+        public WaysCounter(byte[] chocolate)
+        {
+            _chocolate = chocolate;
+        }
+
+        public int Count(byte d, byte m)
+        {
+            if (m > _chocolate.Length || d > m * 5)
+            {
+                return 0;
+            }
+
+            var sum = GetInititalSum(m);
+            var count = (sum == d)?1:0;
+            for(var i=m; i<_chocolate.Length; i++)
+            {
+                sum = sum - _chocolate[i-m] + _chocolate[i];
+                if (sum == d)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+
+        private int GetInititalSum(byte count)
+        {
+            return _chocolate.Take(count).Sum(x=>x);
+        }
+    }
+}
