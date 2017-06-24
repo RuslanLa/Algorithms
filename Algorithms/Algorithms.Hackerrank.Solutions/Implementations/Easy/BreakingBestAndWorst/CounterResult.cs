060da11e1af5c0ae59b0dea3158a92ef17677807
RuslanLatypov <rus_91.08@mail.ru>
2017-06-24 16:54:06 +0300

    Created hackerrank breaking best and worst solution

---
+++ Algorithms/Algorithms.Hackerrank.Solutions/Implementations/Easy/BreakingBestAndWorst/CounterResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Hackerrank.Solutions.Implementations.Easy
+{
+    /// <summary>
+    /// Result of the best and worst breakings counter
+    /// </summary>
+    public class CounterResult
+    {
+        /// <summary>
+        /// Best count
+        /// </summary>
+        public readonly int Best;
+
+        /// <summary>
+        /// Worst count
+        /// </summary>
+        public readonly int Worst;
+
+        public CounterResult(int worst, int best)
+        {
+            Best = best;
+            Worst = worst;
+        }
+    }
+}
