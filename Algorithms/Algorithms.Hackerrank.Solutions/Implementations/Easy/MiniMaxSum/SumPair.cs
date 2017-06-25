558803c4400ffb6677f2d5dad556953099243b11
RuslanLatypov <rus_91.08@mail.ru>
2017-06-25 12:00:32 +0300

    mini max sum challenge solved

---
+++ Algorithms/Algorithms.Hackerrank.Solutions/Implementations/Easy/MiniMaxSum/SumPair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Hackerrank.Solutions.Implementations.Easy.MiniMaxSum
+{
+    public class SumPair
+    {
+        public readonly long Max;
+        public readonly long Min;
+
+        public SumPair(long sum, uint min, uint max)
+        {
+            Max = sum - min;
+            Min = sum - max;
+        }
+    }
+}
