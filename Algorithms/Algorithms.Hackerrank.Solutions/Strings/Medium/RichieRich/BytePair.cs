bd0e0c08231d8f24e59d16072f1f3cded646cf64
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-07-03 14:06:19 +0300

    richie-rich solved

---
+++ Algorithms/Algorithms.Hackerrank.Solutions/Strings/Medium/RichieRich/BytePair.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Hackerrank.Solutions.Strings.Medium.RichieRich
+{
+    /// <summary>
+    /// Helper structure
+    /// Pare of byte values
+    /// </summary>
+    public class BytePair
+    {
+        public byte FirstItem { get; private set; }
+        public byte SecondItem { get; private set; }
+        public readonly int PairNum;
+        public readonly bool ContainsSecondItem;
+        public bool IsEqual { get; private set; }
+
+        public BytePair(char[] pair, int pairNum, int pairsCount, bool isOneItemLastPair)
+        {
+            ContainsSecondItem = (pairNum != pairsCount - 1) || !isOneItemLastPair;
+            FirstItem = Byte.Parse(pair[0].ToString());
+            if (ContainsSecondItem)
+            {
+                SecondItem = Byte.Parse(pair[1].ToString());
+            }
+            PairNum = pairNum;
+            IsEqual = !ContainsSecondItem || FirstItem == SecondItem;
+        }
+
+        public void SetPairValue(byte value)
+        {
+            FirstItem = SecondItem = value;
+            IsEqual = true;
+        }
+    }
+}
