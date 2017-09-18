9bc3c68620bb99b025268fe6d3b5a1a80caf3643
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-09-18 09:58:24 +0300

    Added hamming code implementation

---
+++ Algorithms/Algorithms.Implementations.Tests/HamingCode/CheckerTest.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Hackerrank.Solutions.HammingsCode;
+using Shouldly;
+using Xunit;
+
+
+namespace Algorithms.Implementations.Tests.HamingCode
+{
+    public class CheckerTest
+    {
+        public CheckerTest()
+        {
+            _checker = new Checker();
+        }
+        private readonly Checker _checker;
+
+        [Theory]
+        [InlineData(new bool[] { false, true, true, true, false, false, true, false, true, false, true, false }, null)]
+        [InlineData(new bool[] { false, true, true, true, false, false, true, false, true, false, true, true }, 11)]
+        public void TestCount(bool[] input, int? errorNum)
+        {
+            var result = _checker.Check(input);
+            if(!errorNum.HasValue)
+            result.ShouldBeNull();
+            else
+            result.ShouldBe(errorNum);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations.Tests/HamingCode/EncoderTest.cs
@@ -0,0 +1,27 @@
+﻿using Algorithms.Hackerrank.Solutions.HammingsCode;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.HamingCode
+{
+    public class EncoderTest
+    {
+        public EncoderTest()
+        {
+            _encoder = new Encoder();
+        }
+        private readonly Encoder _encoder;
+
+        [Theory]
+        [InlineData(new bool[]{true, false, false, true, true, false, true, false}, new bool[] {false, true, true, true, false, false, true, false, true, false, true, false})]
+        public void TestCount(bool[] input, bool[] expected)
+        {
+            var result = _encoder.Encode(input);
+            result.Length.ShouldBe(expected.Length);
+            for(var i=0; i<expected.Length; i++)
+            {
+                result[i].ShouldBe(expected[i]);
+            }
+        }
+    }
+}
