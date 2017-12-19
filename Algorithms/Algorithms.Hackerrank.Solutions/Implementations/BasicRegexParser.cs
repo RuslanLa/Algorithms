041853ec39951d290a63acf260488b8ed31928d5
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2017-12-19 17:55:17 +0300

    added basic regex parser solution

---
+++ Algorithms/Algorithms.Hackerrank.Solutions/Implementations/BasicRegexParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Hackerrank.Solutions.Implementations
+{
+    /// <summary>
+    /// https://www.pramp.com/question/KvZ3aL35Ezc5K9Eq9Llp
+    /// Implement a regular expression function isMatch that supports the '.' and '*' symbols.
+    ///  The function receives two strings - text and pattern - and should return true if the text
+    ///  matches the pattern as a regular expression. For simplicity, assume that the actual symbols '.' 
+    /// and '*' do not appear in the text string and are used as special symbols only in the pattern string.
+    /// </summary>
+    public class BasicRegexParser
+    {
+        public bool IsMatch(string text, string pattern)
+        {
+            return IsMatch(text, pattern, 0, 0);
+        }
+
+        private bool IsEndOfTheText(string text, int textIndex) => textIndex >= text.Length;
+        private bool IsAsteriskAfterCurrent(string text, int textIndex) => textIndex < text.Length - 1 && text[textIndex+1] == '*';
+        private bool IsPoint(string pattern, int patternIndex) => pattern[patternIndex] == '.';
+        private bool IsMatch(string text, string pattern, int textIndex, int patternIndex)
+        {
+            var isEndOfTheText = IsEndOfTheText(text, textIndex);
+            var isEndOfThePattern = IsEndOfTheText(pattern, patternIndex);
+            var isAsteriskAfterCurrent = IsAsteriskAfterCurrent(pattern, patternIndex);
+            if ((isEndOfThePattern && isEndOfTheText) || (isEndOfTheText &&
+                                                          IsAsteriskAfterCurrent(pattern, patternIndex) &&
+                                                          (patternIndex + 2 == pattern.Length || IsPoint(pattern, patternIndex))))
+            {
+                return true;
+            }
+
+            if (isEndOfTheText || isEndOfThePattern)
+            {
+                return false;
+            }
+
+            if (text[textIndex] == pattern[patternIndex] || IsPoint(pattern, patternIndex))
+            {
+                if (isAsteriskAfterCurrent)
+                {
+                    return IsMatch(text, pattern, textIndex + 1, patternIndex);
+                }
+                return IsMatch(text, pattern, textIndex + 1, patternIndex + 1);
+            }
+
+            if (isAsteriskAfterCurrent)
+            {
+                return IsMatch(text, pattern, textIndex, patternIndex + 2);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations.Tests/HackerRankSolutions/BasicRegexParserTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Hackerrank.Solutions.Implementations;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.HackerRankSolutions
+{
+    public class BasicRegexParserTests
+    {
+        private BasicRegexParser _parser { get; set; }
+
+        public BasicRegexParserTests()
+        {
+            _parser = new BasicRegexParser();
+        }
+
+        [Theory]
+        [InlineData("abaa", "a.*a*", true)]
+        public void TestParse(string text, string pattern, bool expectedResult)
+        {
+            var actualResult = _parser.IsMatch(text, pattern);
+            actualResult.ShouldBe(expectedResult);
+        }
+
+    }
+}
