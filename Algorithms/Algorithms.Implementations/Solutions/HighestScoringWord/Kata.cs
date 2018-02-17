e20188125c990655ff3a93e44f3ef3b6e71bdfb7
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-02-17 12:46:38 +0300

    2. Highest scoring word

---
+++ Algorithms/Algorithms.Implementations/Solutions/HighestScoringWord/Kata.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// #50KataChallenge
+/// Solution for the https://www.codewars.com/kata/highest-scoring-word/train/csharp
+/// </summary>
+public class Kata
+{
+    public static string High(string s)
+    {
+        var words = s.Split(' ').Select(w => new {Word = w, Value = EvaluateWord(w)}).ToList();
+        var max = words.Max(w => w.Value);
+        return words.Where(w => w.Value == max).Select(w => w.Word).FirstOrDefault();
+    }
+
+    private static int _alphabetBegin = Convert.ToInt32('a') - 1;
+
+    private static int EvaluateWord(string word) => GetCharactersEnumeration(word).Sum();
+
+    private static IEnumerable<int> GetCharactersEnumeration(string word)
+    {
+        foreach (var character in word)
+        {
+            yield return (byte)(character - _alphabetBegin);
+        }
+    }
+
+}
