c9a5eb714ddace76db55002d069b581469bf0f7a
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-02-19 16:32:41 +0300

    4. The BIG Pronic challenge

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/BigPronic/PronicDetectorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.BigPronic;
+using Algorithms.Implementations.Solutions.HammingsCode;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.BigPronic
+{
+    public class PronicDetectorTest
+    {
+        public PronicDetectorTest()
+        {
+            _detector = new PronicDetector();
+        }
+        private readonly PronicDetector _detector;
+
+        [Theory]
+        [InlineData(1152921507828072449, false)]
+        [InlineData(long.MinValue, false)]
+        public void TestCount(long n, bool expected)
+        {
+            _detector.IsPronic(n).ShouldBe(expected);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/BigPronic/PronicDetector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementations.Solutions.BigPronic
+{
+    /// <summary>
+    /// Solution for https://www.codewars.com/kata/the-big-pronic-challenge/csharp
+    /// </summary>
+    public class PronicDetector
+    {
+        public bool IsPronic(long n)
+        {
+
+            if (n < 0 || n % 2 != 0)
+            {
+                return false;
+            }
+            var number = (long) Math.Floor(Math.Sqrt(n));
+            return (number + 1) * number == n;
+        }
+    }
+}
