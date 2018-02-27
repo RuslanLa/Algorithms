890d5240bcd502f73afbfb9fff4241923d6a284b
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-02-27 09:56:20 +0300

    11. Total increasing or decreasing numbers up to a power of 10

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/NonBouncyNumbers/CounterTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.NonBouncyNumbers;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.NonBouncyNumbers
+{
+    public class CounterTests
+    {
+        private readonly Counter _counter;
+        public CounterTests()
+        {
+            _counter = new Counter();
+        }
+
+        [Theory]
+        [InlineData(3, 475)]
+        public void Test(int x, int expectedResult)
+        {
+            _counter.CalculateTotal(x).ShouldBe((BigInteger)expectedResult);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/NonBouncyNumbers/Kata.cs
@@ -0,0 +1,82 @@
+﻿using System.Numerics;
+
+/// <summary>
+/// Solution for kata https://www.codewars.com/kata/55b195a69a6cc409ba000053/csharp
+/// </summary>
+public class TotalIncreasingOrDecreasingNumbers
+{
+    public static BigInteger TotalIncDec(int x)
+    {
+        return new Counter().CalculateTotal(x);
+        // Good Luck!
+    }
+
+    private class Counter
+    {
+        public BigInteger CalculateTotal(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+
+            if (x == 1)
+            {
+                return 10;
+            }
+
+            var increasing = GetIncreasing(x);
+            var decreasing = GetDecreasing(x);
+            var sum = increasing + decreasing;
+            return sum - (x) * 9 - 1;
+        }
+
+
+        private BigInteger GetCount(int x, bool isIncreasing)
+        {
+            var counterArray = GetCounterArray(x, isIncreasing ? 9 : 10);
+            BigInteger count = isIncreasing ? 55 : 64;
+            for (int i = 1; i < x - 1; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    BigInteger value = 0;
+                    for (int z = j; z < 9; z++)
+                    {
+                        value += counterArray[i - 1, z];
+                    }
+
+                    if (!isIncreasing)
+                    {
+                        value += 1;
+                    }
+                    counterArray[i, j] = value;
+                    count += value;
+                }
+            }
+
+            return count;
+        }
+
+        private BigInteger GetIncreasing(int x)
+        {
+            return GetCount(x, true);
+        }
+
+        private BigInteger GetDecreasing(int x)
+        {
+            return GetCount(x, false);
+        }
+
+        private BigInteger[,] GetCounterArray(int x, int possibleCounts)
+        {
+            var counterArray = new BigInteger[x - 1, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                counterArray[0, i] = possibleCounts - i;
+            }
+
+            return counterArray;
+        }
+    }
+}
