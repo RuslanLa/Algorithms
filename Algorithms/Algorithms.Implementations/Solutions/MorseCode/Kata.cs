de6909c0bd89aed1181c63a0b0bf9778ba83e858
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-03-06 15:31:00 +0300

    16. decode-the-morse-code

---
+++ Algorithms/Algorithms.Implementations/Solutions/MorseCode/Kata.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Linq;
+
+/// <summary>
+/// Solution for the KATA https://www.codewars.com/kata/decode-the-morse-code
+/// </summary>
+//class MorseCodeDecoder
+//{
+//    public static string Decode(string morseCode)
+//    {
+//        return String.Join(" ", morseCode.Split(new[] { "   " }, StringSplitOptions.None)
+//            .Where(w => !String.IsNullOrEmpty(w))
+//            .Select(w => String.Join("", w.Split(' ').Where(c => !String.IsNullOrEmpty(c)).Select(MorseCode.Get))));
+//    }
+//}
---
+++ Algorithms/Algorithms.Implementations/Solutions/MorseCode/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Implementations.Solutions.MorseCode
+{
+    /// <summary>
+    /// Char Code mock - related to task on codewars structure
+    /// </summary>
+    public static class MorseCode
+    {
+        public static string Get(string code)
+        {
+            return String.Empty;
+        }
+    }
+
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/decode-the-morse-code
+    /// </summary>
+    public static class StringExtensions
+    {
+        public static string Decode(string code)
+        {
+            return String.Join(" ", code.Split(new[] {"   "}, StringSplitOptions.None)
+                .Where(w => !String.IsNullOrEmpty(w))
+                .Select(w => String.Join("", w.Split(' ').Where(c => !String.IsNullOrEmpty(c)).Select(MorseCode.Get))));
+        }
+    }
+}
