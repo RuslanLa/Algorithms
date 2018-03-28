495340cfdeb69740ac2745ea56824625d272f976
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-03-28 16:37:21 +0300

    21. Graphics 02: Drawing Circles

---
+++ Algorithms/Algorithms.Implementations/Solutions/Graphics/Circle/Kata.cs
@@ -0,0 +1,68 @@
+﻿//using System.Collections.Generic;
+//using System.Linq;
+
+//namespace smile67Kata
+//{
+//    using System;
+//    public class Kata
+//    {
+//        private class Point
+//        {
+//            public Point(int x, int y)
+//            {
+//                X = x;
+//                Y = y;
+//            }
+//            public int X { get; }
+//            public int Y { get; }
+//            public override bool Equals(object obj)
+//            {
+//                if (!(obj is Point))
+//                {
+//                    return false;
+//                }
+
+//                var point = obj as Point;
+//                return point.X == this.X && point.Y == this.Y;
+//            }
+
+//            public override int GetHashCode()
+//            {
+//                return X ^ Y;
+//            }
+//        }
+
+//        private static bool IsPointHidden(int x, int y)
+//        {
+//            return x > 100 || x < 0 || y > 50 || y < 0;
+//        }
+
+//        private static IEnumerable<Point> GetCirclePointsForX(int xCenter, int yCenter, int x, int r)
+//        {
+//            var yPoint = (int)Math.Sqrt(r * r - x * x);
+//            yield return new Point(x, yPoint);
+//            yield return new Point((xCenter - x) + xCenter, yPoint);
+//            yield return new Point(x, (yCenter - yPoint) + yCenter);
+//            yield return new Point((xCenter - x) + xCenter, (yCenter - yPoint) + yCenter);
+//        }
+
+//        private static IEnumerable<Point> GetVisiblePartOfCircle(int x, int y, int r)
+//        {
+//            return GetAllCirclePoints(x, y, r).Where(p => !IsPointHidden(p.X, p.Y));
+//        }
+
+//        private static IEnumerable<Point> GetAllCirclePoints(int x, int y, int r)
+//        {
+//            return Enumerable.Range(x - r, r + 1).SelectMany(i => GetCirclePointsForX(x, y, i, r)).Distinct();
+//        }
+
+
+//        public void drawCircle(int x, int y, int r)
+//        {
+//            foreach (var point in GetVisiblePartOfCircle(x, y, r))
+//            {
+//                Drawing.Canvas[point.X, point.Y] = true;
+//            }
+//        }
+//    }
+//}
--- Algorithms/Algorithms.Implementations/Solutions/Graphics/Drawer.cs
+++ Algorithms/Algorithms.Implementations/Solutions/Graphics/Drawer.cs
@@ -1,10 +1,42 @@
-﻿namespace Algorithms.Implementations.Solutions.Graphics
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Algorithms.Implementations.Solutions.Graphics
 {
     /// <summary>
     /// Solution for http://www.codewars.com/kata/569e0778f3b6ef81b7000046/csharp
     /// </summary>
     public static class Drawer
     {
+        public class Point
+        {
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public  int X { get;}
+            public int Y { get; }
+            public override bool Equals(object obj)
+            {
+                if (!(obj is Point))
+                {
+                    return false;
+                }
+
+                var point = obj as Point;
+                return point.X == this.X && point.Y == this.Y;
+            }
+
+            public override int GetHashCode()
+            {
+                return X ^ Y;
+            }
+        }
+
         public static bool[,] Line(int x1, int y1, int x2, int y2)
         {
             var coeffeicient = CalculateCoeffecient(x1, y1, x2, y2);
@@ -35,6 +67,31 @@
         private static double CalculateSummand(int x1, int y1, double k)
         {
             return y1 - k * x1;
+        }
+
+
+        private static bool IsPointHidden(int x, int y)
+        {
+            return x > 100 || x < 0 || y > 50 || y < 0;
+        }
+
+        private static IEnumerable<Point> GetCirclePointsForX(int xCenter, int yCenter, int x, int r)
+        {
+            var yPoint = (int)Math.Sqrt(r*r - x * x);
+            yield return new Point(x, yPoint);
+            yield return new Point((xCenter - x) + xCenter, yPoint);
+            yield return new Point(x, (yCenter - yPoint) + yCenter);
+            yield return new Point((xCenter - x) + xCenter, (yCenter - yPoint) + yCenter);
+        }
+
+        public static IEnumerable<Point> GetVisiblePartOfCircle(int x, int y, int r)
+        {
+            return GetAllCirclePoints(x, y, r).Where(p => !IsPointHidden(p.X, p.Y));
+        }
+
+        private static IEnumerable<Point> GetAllCirclePoints(int x, int y, int r)
+        {
+            return Enumerable.Range(x - r, r + 1).SelectMany(i => GetCirclePointsForX(x, y, i, r)).Distinct();
         }
     }
 }
