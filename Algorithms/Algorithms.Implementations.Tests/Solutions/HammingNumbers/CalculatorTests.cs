51a0f6864b2183a6640116cf355f651d5863fe0d
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-03-29 18:19:13 +0300

    22. Hamming Numbers

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/HammingNumbers/CalculatorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.HammingNumbers;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.HammingNumbers
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _hammingNumberCalculator = new Calculator();
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 4)]
+        [InlineData(5, 5)]
+        [InlineData(6, 6)]
+        [InlineData(7, 8)]
+        [InlineData(8, 9)]
+        [InlineData(9, 10)]
+        public void CalculateNth(int n, long expected)
+        {
+            _hammingNumberCalculator.CalculateNth(n).ShouldBe(expected);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/HammingNumbers/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.HammingNumbers
+{
+    public class Calculator
+    {
+        public long CalculateNth(int n)
+        {
+            var uglies = new long[n];
+            uglies[0] = 1;
+            uint lastIndex2 = 0;
+            uint lastIndex3 = 0;
+            uint lastIndex5 = 0;
+            long nextValue2 = 2;
+            long nextValue3 = 3;
+            long nextValue5 = 5;
+
+            for (uint i = 1; i < n; i++)
+            {
+                var minValue = Min(nextValue2, nextValue3, nextValue5);
+                uglies[i] = minValue;
+                if (nextValue2 == minValue)
+                {
+                    RememberValue(uglies, 2, ref lastIndex2, ref nextValue2);
+                }
+
+                if (nextValue3 == minValue)
+                {
+                    RememberValue(uglies, 3, ref lastIndex3, ref nextValue3);
+                }
+
+                if (nextValue5 == minValue)
+                {
+                    RememberValue(uglies, 5, ref lastIndex5, ref nextValue5);
+                }
+            }
+
+            return uglies[n - 1];
+        }
+
+        private long Min(params long[] values)
+        {
+            return values.Min();
+        }
+
+        private static void RememberValue(long[] uglies, uint multiplicator, ref uint valueIndex, ref long nextValue)
+        {
+            valueIndex++;
+            nextValue = uglies[valueIndex] * multiplicator;
+        }
+    }
+}
+
