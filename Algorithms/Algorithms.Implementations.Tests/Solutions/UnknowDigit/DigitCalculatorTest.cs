0bf9fadd59aec767ad068d6f3c4ba969eb3fbb68
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-02 09:54:21 +0300

    24 - Find the unknown digit

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/UnknowDigit/DigitCalculatorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.UnknownDigit;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.UnknowDigit
+{
+    public class DigitCalculatorTest
+    {
+        [Theory]
+        [InlineData("1+1=?", 2)]
+        [InlineData("123*45?=5?088", 6)]
+        [InlineData("123?45*?=?", 0)]
+        [InlineData("123?45-?=123?45", 0)]
+        [InlineData("??605*-63=-73???5",1)]
+        public void Calculate(string expression, int result)
+        {
+            new DigitCalculator().Calculate(expression).ShouldBe(result);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/UnknownDigit/Kata.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Runes
+{
+    public static int solveExpression(string expression)
+    {
+        return new DigitCalculator().Calculate(expression);
+    }
+
+    private class DigitCalculator
+    {
+        public int Calculate(string input)
+        {
+            var splitted = input.Split('=');
+            if (splitted.Length < 2)
+            {
+                return -1;
+            }
+
+            var unique = Enumerable.Range(0, 10).Where(x => !input.Contains(x.ToString()));
+            foreach (byte i in unique)
+            {
+                var expression = CalculateExpression(FillUnknowDigit(splitted[0], i));
+                int expectedResult;
+                if (!ToInteger(FillUnknowDigit(splitted[1], i), out expectedResult))
+                {
+                    continue;
+                }
+                if (expression == expectedResult)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private string FillUnknowDigit(string input, byte d)
+        {
+            return input.Replace("?", d.ToString());
+        }
+
+        private int? CalculateExpression(string input)
+        {
+            var i = 0;
+            var summands = new List<int>();
+            var isMinus = false;
+            int? number = null;
+            var isMultiply = false;
+            while (i < input.Length)
+            {
+                if (input[i] == '-')
+                {
+                    if (!isMultiply && number.HasValue)
+                    {
+                        summands.Add(number.Value);
+                        number = null;
+                    }
+
+                    isMinus = !isMinus;
+                    i++;
+                    continue;
+                }
+                if (input[i] == '*')
+                {
+                    isMultiply = true;
+                    i++;
+                    continue;
+                }
+
+                if (input[i] == '+')
+                {
+                    summands.Add(number.Value);
+                    number = null;
+                    i++;
+                    continue;
+                }
+
+                if (Char.IsDigit(input[i]))
+                {
+                    int value;
+                    if (!ToInteger(input, ref i, out value))
+                    {
+                        return null;
+                    }
+                    value = value * (isMinus ? -1 : 1);
+                    if (isMultiply)
+                    {
+                        if (!number.HasValue)
+                        {
+                            throw new Exception("unexpected value");
+                        }
+                        value = number.Value * value;
+                    }
+
+                    number = value;
+                    isMultiply = false;
+                    isMinus = false;
+                    continue;
+                }
+
+                i++;
+            }
+
+            return summands.Sum(x => x) + (number ?? 0);
+        }
+
+        private bool ToInteger(string input, out int result)
+        {
+            int i = 0;
+            return ToInteger(input, ref i, out result);
+        }
+
+        private bool ToInteger(string input, ref int index, out int result)
+        {
+            var number = 0;
+            var digitNum = 0;
+            var isMinus = false;
+            if (input[index] == '-')
+            {
+                index++;
+                isMinus = true;
+            }
+            while (index < input.Length && Char.IsDigit(input[index]))
+            {
+                number = number * 10 + (int)Char.GetNumericValue(input[index]);
+                digitNum++;
+                if (digitNum == 2 && number == 0)
+                {
+                    result = number;
+                    return false;
+                }
+                index++;
+            }
+
+            result = number * (isMinus ? -1 : 1);
+            return true;
+        }
+    }
+}
