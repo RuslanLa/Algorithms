4547f24e85e07719deecc9b15b868ba8866378af
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-02 16:30:55 +0300

    26 - Fun with trees: is perfect

---
+++ Algorithms/Algorithms.Implementations/Solutions/PerfectTree/TreeNode.cs
@@ -0,0 +1,85 @@
+﻿namespace Algorithms.Implementations.Solutions.PerfectTree
+{
+    /// <summary>
+    /// Solution for the kata https://www.codewars.com/kata/fun-with-trees-is-perfect
+    /// class structure was inherited from the kata
+    /// </summary>
+    public class TreeNode
+    {
+        public TreeNode left;
+        public TreeNode right;
+
+        public static bool IsPerfect(TreeNode root)
+        {
+            if (root == null) return true;
+            return root.GetLeavesLevel() != null; // TODO: implementation
+        }
+
+
+        private int? GetLeavesLevel()
+        {
+            if (this.left == null || this.right == null)
+            {
+                if (this.left == this.right)
+                {
+                    return 0;
+                }
+
+                return null;
+            }
+
+            var leftChildLevel = this.left.GetLeavesLevel();
+            var rightChildLevel = this.right.GetLeavesLevel();
+            if (leftChildLevel != rightChildLevel)
+            {
+                return null;
+            }
+
+            return leftChildLevel + 1;
+        }
+
+        public static TreeNode Leaf()
+        {
+            return new TreeNode();
+        }
+
+        public static TreeNode Join(TreeNode left, TreeNode right)
+        {
+            return new TreeNode().WithChildren(left, right);
+        }
+
+        public TreeNode WithLeft(TreeNode left)
+        {
+            this.left = left;
+            return this;
+        }
+
+        public TreeNode WithRight(TreeNode right)
+        {
+            this.right = right;
+            return this;
+        }
+
+        public TreeNode WithChildren(TreeNode left, TreeNode right)
+        {
+            this.left = left;
+            this.right = right;
+            return this;
+        }
+
+        public TreeNode WithLeftLeaf()
+        {
+            return WithLeft(Leaf());
+        }
+
+        public TreeNode WithRightLeaf()
+        {
+            return WithRight(Leaf());
+        }
+
+        public TreeNode WithLeaves()
+        {
+            return WithChildren(Leaf(), Leaf());
+        }
+    }
+}
