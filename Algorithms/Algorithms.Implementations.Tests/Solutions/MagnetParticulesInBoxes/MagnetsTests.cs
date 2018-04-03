8b8e94ddbe9c455d9b59d09404c121651b9cbb0b
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-03 10:13:40 +0300

    27 - Magnet particules in boxes

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/MagnetParticulesInBoxes/MagnetsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.MagnetParticulesInBoxes;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.MagnetParticulesInBoxes
+{
+    public class MagnetsTests
+    {
+        private Magnets _magnets = new Magnets();
+        [Theory]
+        [InlineData(1, 10, 0.5580321939764581)]
+        [InlineData(10, 1000, 0.6921486500921933)]
+        public void DoubleTest(int maxk, int maxn, double expectedResult)
+        {
+            _magnets.Doubles(maxk, maxn).ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/MagnetParticulesInBoxes/Magnets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementations.Solutions.MagnetParticulesInBoxes
+{
+    public class Magnets
+    {
+        public double Doubles(int maxk, int maxn)
+        {
+            double sum = 0;
+            for (int i = 1; i <= maxk; i++)
+            {
+                for (int j = 1; j <= maxn; j++)
+                {
+                    sum += (double) 1 / (i * Math.Pow(j + 1, 2*i));
+                }
+            }
+
+            return sum;
+        }
+    }
+}
