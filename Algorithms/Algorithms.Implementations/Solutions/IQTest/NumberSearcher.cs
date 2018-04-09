bc7dec624c378c90b1a1e3168d479b3e8e2de942
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-09 11:02:31 +0300

    30 - IQ test

---
+++ Algorithms/Algorithms.Implementations/Solutions/IQTest/NumberSearcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.IQTest
+{
+    public class NumberSearcher
+    {
+        public int GetPositionOfDifferentNumber(string inp)
+        {
+            var groups = inp.Split(' ').Select((v, i) => new {v = Int32.Parse(v), i}).GroupBy(x => x.v % 2)
+                .OrderBy(g => g.Count()).Select(g => g.FirstOrDefault())
+                .ToList();
+            return groups.Count < 2 ? -1 : groups[0].i + 1;
+        }
+    }
+}
