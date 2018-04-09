a5bf5e232922834240f0b810d9e9c10c6b720c31
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-09 11:02:19 +0300

    29 - Next smaller pronic

---
+++ Algorithms/Algorithms.Implementations/Solutions/NextSmallerPronic/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Algorithms.Implementations.Solutions.NextSmallerPronic
+{
+    /// <summary>
+    /// Solution for the Kata  https://www.codewars.com/kata/next-smaller-pronic
+    /// </summary>
+    public class Calculator
+    {
+        public ulong CalculateNextSmallerPronic(ulong x)
+        {
+            var n = (ulong) System.Math.Sqrt(x);
+            Console.WriteLine(n);
+            var first = n * (n - 1);
+            var second = n * (n + 1);
+            return second < x ? second : first;
+        }
+    }
+}
