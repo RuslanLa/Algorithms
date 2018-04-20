ca71cefcde4fe24390295c26b0ace94bf22181d5
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-20 13:29:07 +0300

    34 - Digital Root

---
+++ Algorithms/Algorithms.Implementations/Solutions/DigitalRoot/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementations.Solutions.DigitalRoot
+{
+    public class Number
+    {
+        public int DigitalRoot(long n)
+        {
+            while (n > 9)
+            {
+                n = GetDigits(n).Sum();
+            }
+
+            return (int)n;
+        }
+
+        private IEnumerable<int> GetDigits(long n)
+        {
+            while (n!=0)
+            {
+                var remainder = (int)(n % 10);
+                n /= 10;
+                yield return remainder;
+            }
+        }
+    }
+}
