3d1c6e450d6fbc1c24bcd73dc899f239b92f0b20
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-25 18:56:27 +0300

    37 - MovingZerosToTheEnd

---
+++ Algorithms/Algorithms.Implementations/Solutions/MovingZerosToTheEnd/Kata.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// The solution for the kata https://www.codewars.com/kata/moving-zeros-to-the-end
+/// </summary>
+public class Kata
+{
+    public static int[] MoveZeroes(int[] arr)
+    {
+        return arr.Where(x => x != 0).Concat(Enumerable.Repeat(0, arr.Length)).Take(arr.Length).ToArray();
+    }
+}
