d9f24b713031d6e29ddb7f68e717a404136678c0
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-25 21:54:44 +0300

    38 - LongestCommonSequence

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/LongestCommonSequence/SubsequenceSearcherTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.LongestCommonSubsequence;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.LongestCommonSequence
+{
+    public class SubsequenceSearcherTests
+    {
+        private readonly SubsequenceSearcher _subsequenceSearcher = new SubsequenceSearcher();
+
+        [Theory]
+        [InlineData(";RBEDE>NG1>96O9G?690?33;B7<G?66?1BN4BIG0:@B93@IPL@1G<:48O62<", "8K?ANR8H154JB9JRDM82A@NR5B3J=F8;F:KC10>481OG;:42D5G2R0OLP@<;", "RBDN1>OG;4G0P@<")]
+        [InlineData("nothardlythefinaltest", "zzzfinallyzzz", "final")]
+        public void Search(string a, string b, string expected)
+        {
+            _subsequenceSearcher.Find(a, b).ShouldBe(expected);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/LongestCommonSubsequence/Kata.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class LcsClass
+{
+    public static string Lcs(string a, string b)
+    {
+        return Find(a, b); // do it!
+    }
+
+    private class SubsequenceData
+    {
+        public int EndsOn { get; set; }
+        public char[] Sequence { get; set; }
+    }
+    private static Dictionary<char, int[]> DemountString(string input)
+    {
+        return input.Select((v, i) => new { v, i }).GroupBy(x => x.v).
+            ToDictionary(g => g.Key, g => g.Select(x => x.i).ToArray());
+    }
+    private static string Find(string a, string b)
+    {
+        var sequences = new List<SubsequenceData>();
+        var biggestLength = 0;
+        char[] biggestSequence = null;
+        var characters = DemountString(b);
+        foreach (var character in a.ToCharArray())
+        {
+            if (!characters.ContainsKey(character))
+            {
+                continue;
+            }
+            var sequencesCopy = sequences.ToArray();
+
+            foreach (var position in characters[character])
+            {
+                var prevInfo = FindPreviousSubsequence(position, sequencesCopy) ?? new SubsequenceData() { Sequence = new char[0] };
+                var newSequence = prevInfo.Sequence.Concat(new[] { character }).ToArray();
+                if (newSequence.Length > biggestLength)
+                {
+                    biggestLength = newSequence.Length;
+                    biggestSequence = newSequence;
+                }
+                sequences.Add(new SubsequenceData()
+                {
+                    EndsOn = position,
+                    Sequence = newSequence
+                });
+            }
+        }
+
+        if (biggestLength == 0)
+        {
+            return String.Empty;
+        }
+        return new string(biggestSequence);
+    }
+
+    private static SubsequenceData FindPreviousSubsequence(int index, SubsequenceData[] subsequences) =>
+        subsequences.Where(s => s.EndsOn < index).OrderByDescending(s => s.Sequence.Length).FirstOrDefault();
+}
