44680f1523b0cf5faf3428f93601e33a94488093
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-04-30 10:19:01 +0300

    41 - TwiceLinear

---
+++ Algorithms/Algorithms.Implementations/Solutions/TwiceLinear/Kata.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// Solution for the KATA https://www.codewars.com/kata/twice-linear
+/// </summary>
+public class DoubleLinear
+{
+    public static int DblLinear(int n)
+    {
+        var sequence = new List<int>(n) { 1 };
+        var twiceIndex = 0;
+        var tripleIndex = 0;
+        while (sequence.Count <= n)
+        {
+            var twice = sequence[twiceIndex] * 2 + 1;
+            var tripple = sequence[tripleIndex] * 3 + 1;
+            if (twice == tripple)
+            {
+                tripleIndex++;
+            }
+
+            if (twice <= tripple)
+            {
+                twiceIndex++;
+                sequence.Add(twice);
+                continue;
+            }
+
+            tripleIndex++;
+            sequence.Add(tripple);
+        }
+
+        return sequence.Last();
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/TwiceLinear/Sequence.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementations.Solutions.TwiceLinear
+{
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/twice-linear
+    /// </summary>
+    public class Sequence
+    {
+        public int GetNth(int n)
+        {
+            var sequence = new List<int>(n) { 1 };
+            var twiceIndex = 0;
+            var tripleIndex = 0;
+            while (sequence.Count <= n)
+            {
+                var twice = sequence[twiceIndex] * 2 + 1;
+                var tripple = sequence[tripleIndex] * 3 + 1;
+                if (twice == tripple)
+                {
+                    tripleIndex++;
+                }
+
+                if (twice <= tripple)
+                {
+                    twiceIndex++;
+                    sequence.Add(twice);
+                    continue;
+                }
+
+                tripleIndex++;
+                sequence.Add(tripple);
+            }
+
+            return sequence.Last();
+        }
+    }
+}
