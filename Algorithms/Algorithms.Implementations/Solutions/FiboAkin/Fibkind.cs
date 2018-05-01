b63d8801ed0516da5dc9d3411db1ca74db9112ef
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-01 18:28:44 +0300

    43 - Fibo Akin

---
+++ Algorithms/Algorithms.Implementations/Solutions/FiboAkin/Fibkind.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.FiboAkin
+{
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/fibo-akin
+    /// </summary>
+    public class Fibkind
+    {
+        public static long LengthSupUK(int n, int k)
+        {
+            var sequence = Generate(n);
+            return sequence.LongCount(x => x >= k);
+        }
+
+        public static long Comp(int n)
+        {
+            var sequence = Generate(n);
+            return sequence.Select((v, i) => new {cur = v, prev = i == 0 ? 0 : sequence[i - 1]}).LongCount(x => x.cur < x.prev);
+        }
+
+        private static long[] Generate(int length)
+        {
+            var sequence = new long[length];
+            sequence[0] = 1;
+            sequence[1] = 1;
+            for (int i = 2; i < length; i++)
+            {
+                sequence[i] = sequence[i - sequence[i - 1]] + sequence[i - sequence[i - 2]];
+            }
+
+            return sequence;
+        }
+
+    }
+}
