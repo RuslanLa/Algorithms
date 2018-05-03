c6b5ece444d65d86085dbad89cc9fe7edee013e6
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-03 10:52:02 +0300

    44 - Packing your backpack

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/PackingABackpack/PackerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.PackingABackpack;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.PackingABackpack
+{
+    public class PackerTests
+    {
+        private readonly Packer _packer = new Packer();
+
+        [Theory]
+        [InlineData(new int[] { 15, 10, 9, 5 }, new int[] { 1, 5, 3, 4 }, 8, 29)]
+        public void GetMaxBagScore(int[] scores, int[] weights, int capacity, int expected)
+        {
+            _packer.GetMaxBagScore(scores, weights, capacity).ShouldBe(expected);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/PackingABackpack/Packer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.PackingABackpack
+{
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/5a51717fa7ca4d133f001fdf
+    /// (Knapsack problem without repititions)
+    /// </summary>
+    public class Packer
+    {
+        public int GetMaxBagScore(int[] scores, int[] weight, int capacity)
+        {
+            var maxScores = new int[capacity];
+            var usedForCapacities = new List<int>[capacity]; 
+            for (int i = 0; i < capacity; i++)
+            {
+                var maxScore = i == 0 ? 0 : maxScores[i - 1];
+                var usedThings = i == 0 ? new List<int>(){} : usedForCapacities[i - 1]; 
+                for (int j = 0; j < scores.Length; j++)
+                {
+                    var remainWeight = i + 1 - weight[j];
+                    if (remainWeight < 0 || (remainWeight != 0 && usedForCapacities[remainWeight-1].Contains(j)))
+                    {
+                        continue;
+                    }
+
+                    var curScore = (remainWeight==0?0:maxScores[remainWeight-1]) + scores[j];
+                    if (curScore > maxScore)
+                    {
+                        maxScore = curScore;
+                        usedThings = remainWeight == 0
+                            ? new List<int>() {j}
+                            : usedForCapacities[remainWeight - 1].Concat(new[] {j}).ToList();
+                    }
+
+                }
+
+                maxScores[i] = maxScore;
+                usedForCapacities[i] = usedThings;
+            }
+
+            return maxScores[capacity-1];
+        }
+    }
+}
