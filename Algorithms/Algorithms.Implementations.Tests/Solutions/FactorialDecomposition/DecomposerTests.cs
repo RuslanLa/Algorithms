338f24ff260315ab2704d6bd20345e90f326fc65
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-04 16:12:50 +0300

    46 - Factorial decomposition

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/FactorialDecomposition/DecomposerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.FactorialDecomposition;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.FactorialDecomposition
+{
+    public class DecomposerTests
+    {
+        [Theory]
+        [InlineData(22, "2^19 * 3^9 * 5^4 * 7^3 * 11^2 * 13 * 17 * 19")]
+        public void Decompose(int n, string expected)
+        {
+            new Decomposer().Decompose(n).ShouldBe(expected);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/FactorialDecomposition/Kata.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class FactDecomp
+{
+    public static string Decomp(int n)
+    {
+        if (n == 1)
+        {
+            return String.Empty;
+        }
+        var nums = Enumerable.Range(2, n - 1).ToArray();
+        return String.Join(" * ",
+            GetDecomposition(nums).Select(x => x.Value == 1 ? x.Key.ToString() :
+                String.Format("{0}^{1}", x.Key, x.Value)));
+    }
+
+    private static Dictionary<int, int> GetDecomposition(int[] nums)
+    {
+        var result = new Dictionary<int, int>();
+        var last = nums.Last();
+        for (int i = 2; i <= last; i++)
+        {
+            var count = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                while (nums[j] % i == 0)
+                {
+                    count++;
+                    nums[j] /= i;
+                }
+            }
+
+            if (count == 0)
+            {
+                continue;
+            }
+            result.Add(i, count);
+        }
+
+        return result;
+    }
+}
