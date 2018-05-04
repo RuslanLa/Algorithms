2ed3e61667bbaf2c85a2bcd3bd5b23410e646b5c
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-04 15:43:18 +0300

    45 - Buddy Pairs

---
+++ Algorithms/Algorithms.Implementations.Tests/Solutions/BuddyPairs/PairsSearcherTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Algorithms.Implementations.Solutions.BuddyPairs;
+using Shouldly;
+using Xunit;
+
+namespace Algorithms.Implementations.Tests.Solutions.BuddyPairs
+{
+    public class PairsSearcherTests
+    {
+        [Theory]
+        [InlineData(10, 50, "(48 75)")]
+        [InlineData(1071625, 1103735, "(1081184 1331967)")]
+        public void GetDivisors(long start, long limit, string result)
+        {
+            new PairsSearcher().Find(start, limit).ShouldBe(result);
+        }
+    }
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/BuddyPairs/PairsSearcher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.BuddyPairs
+{
+    public class PairsSearcher
+    {
+        public string Find(long start, long limit)
+        {
+            for (var i = start; i <= limit; i++)
+            {
+                var secondNumber = SumDivisors(GetDivisors(i), i) - 1;
+                if (SumDivisors(GetDivisors(secondNumber), secondNumber) - 1 == i && secondNumber>i)
+                {
+                    return String.Format("({0} {1})", i, secondNumber);
+                }
+            }
+
+            return "Nothing";
+        }
+
+        private IEnumerable<long> GetDivisors(long number)
+        {
+            for (long i = 1; i <= (long)Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+
+        private long SumDivisors(IEnumerable<long> divisors, long number) =>
+            divisors.Select(d => new
+            {
+                d,
+                addition = number / d
+            }).Where(x => x.d != x.addition).Select(x => x.d + x.addition).Sum() - number;
+    }
+}
