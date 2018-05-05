df3a8f903d87bb91bf56c9b2fcf66811d462295d
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-05 10:34:42 +0300

    48 - WeightForWeight

---
+++ Algorithms/Algorithms.Implementations/Solutions/WeightForWeight/Kata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// Solution for the KATA https://www.codewars.com/kata/55c6126177c9441a570000cc
+/// </summary>
+public class WeightSort
+{
+    public static string orderWeight(string input)
+    {
+        return String.Join(" ", input.Split(' ').Select(x => x.Trim()).Select(x => new
+        {
+            num = x,
+            sum = CalculateDigitsSum(x)
+        }).OrderBy(x => x.sum).ThenBy(x => x.num).Select(x => x.num));
+    }
+
+    private static int CalculateDigitsSum(string number)
+    {
+        var sum = 0;
+        foreach (var digit in number.Select(x => (int)Char.GetNumericValue(x)))
+        {
+            sum += digit;
+        }
+
+        return sum;
+    }
+
+}
---
+++ Algorithms/Algorithms.Implementations/Solutions/WeightForWeight/WeightListManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Implementations.Solutions.WeightForWeight
+{
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/55c6126177c9441a570000cc
+    /// </summary>
+    public class WeightListManager
+    {
+        public string Reorder(string input)
+        {
+            return String.Join(" ", input.Split(' ').Select(x => x.Trim()).Select(x => new
+            {
+                num = x,
+                sum = CalculateDigitsSum(x)
+            }).OrderBy(x => x.sum).ThenBy(x => x.num).Select(x => x.num));
+        }
+
+        private int CalculateDigitsSum(string number)
+        {
+            var sum = 0;
+            foreach (var digit in number.Select(x=>(int)Char.GetNumericValue(x)))
+            {
+                sum = sum * 10 + digit;
+            }
+
+            return sum;
+        }
+    }
+}
