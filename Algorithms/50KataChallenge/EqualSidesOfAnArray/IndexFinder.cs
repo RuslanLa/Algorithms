84ac633ea2a8a4db150bb993b473e3a420ec506f
Ruslan.Latypov\Akvelon <ruslan.latypov@akvelon.com>
2018-05-05 10:21:02 +0300

    47 - Equal sides of an array

---
+++ Algorithms/50KataChallenge/EqualSidesOfAnArray/IndexFinder.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Algorithms.Implementations.Solutions.EqualSidesOfAnArray
+{
+    /// <summary>
+    /// Solution for the KATA https://www.codewars.com/kata/equal-sides-of-an-array
+    /// </summary>
+    public class IndexFinder
+    {
+        public int FindIndexOfSplit(int[] array)
+        {
+            var leftSum = 0;
+            var rightSum = array.Sum();
+            for (var i = 0; i < array.Length; i++)
+            {
+                rightSum -= array[i];
+                if (rightSum == leftSum)
+                {
+                    return i;
+                }
+
+                leftSum += array[i];
+            }
+            return -1;
+        }
+    }
+}
